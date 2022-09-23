e5231b6372313937b35b5610933751c257438f39
stargateaudio <stargateaudio@noreply.github.com>
2022-09-22 22:04:06 -0700

    Use Qt HiDPI scaling

--- src/pytemplate_qt/main.py
+++ src/pytemplate_qt/main.py
@@ -1,11 +1,22 @@
 import sys
 
 from pytemplate.setup import setup
+from pytemplate.log import LOG
 
 def main():
     # Before Qt imports, to ensure that we can log the outcome
     setup()
-    from .qt import QApplication
+    from .qt import QApplication, QGuiApplication, QtCore
+    try:
+        QGuiApplication.setHighDpiScaleFactorRoundingPolicy(
+            QtCore.Qt.HighDpiScaleFactorRoundingPolicy.PassThrough,
+        )
+    except Exception as ex:
+        LOG.warning(
+            "Unable to set "
+            "QGuiApplication.setHighDpiScaleFactorRoundingPolicy"
+            f" {ex}"
+        )
     from .widgets.main_window import MainWindow
     app = QApplication(sys.argv)
     window = MainWindow()
