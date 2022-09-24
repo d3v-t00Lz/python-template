70529609999260d2f63fb3855c8f8f3d60994c93
stargateaudio <stargateaudio@noreply.github.com>
2022-09-24 10:31:01 -0700

    Add appimage deps

--- tools/debian_devel_deps.sh
+++ tools/debian_devel_deps.sh
@@ -3,5 +3,5 @@
 # Developer tools required to build packages
 
 sudo apt update
-sudo apt install devscripts jq
+sudo apt install devscripts jq python3-dev
 
--- tools/fedora_devel_deps.sh
+++ tools/fedora_devel_deps.sh
@@ -3,5 +3,5 @@
 # Developer tools required to build packages
 
 sudo dnf update
-sudo dnf install jq rpm-build
+sudo dnf install jq rpm-build python3-devel
 
