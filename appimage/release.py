c37ecb619546d30159c4014b3e53fe1730312663
stargateaudio <stargateaudio@noreply.github.com>
2022-09-24 13:57:26 -0700

    Automatically use the current repo
    Eliminate manual steps to update the requirements.txt files

--- appimage/release.py
+++ appimage/release.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import copy
 import os
 import shutil
 import subprocess
@@ -26,25 +27,7 @@ DIRS = tuple(sorted(x for x in os.listdir() if os.path.isdir(x)))
 
 def parse_args():
     parser = argparse.ArgumentParser('Create AppImages')
-    parser.set_defaults(func=lambda x: print('No subcommand chosen!'))
-
-    subparsers = parser.add_subparsers()
-    localrepo_parser = subparsers.add_parser(
-        'localrepo',
-        help=(
-            'Set the AppImages to build from the local reposistory instead '
-            'of the remote repository.  You must commit the changes '
-            'after this.'
-        ),
-    )
-    localrepo_parser.set_defaults(func=localrepo)
-
-    build_parser = subparsers.add_parser(
-        'build',
-        help='Build an AppImage',
-    )
-    build_parser.set_defaults(func=build)
-    build_parser.add_argument(
+    parser.add_argument(
         '-p',
         '--python-version',
         default='3.10',
@@ -57,28 +40,23 @@ def parse_args():
         ),
     )
     if len(DIRS) > 1:
-        build_parser.add_argument(
+        parser.add_argument(
             'dirname',
             choices=DIRS,
             help='The appimage directory to build',
         )
     return parser.parse_args()
 
-def localrepo(args):
-    localpath = os.path.abspath(
+def build(args):
+    DIRNAME = args.dirname if len(DIRS) > 1 else DIRS[0]
+    current_repo = os.path.abspath(
         os.path.join(
             os.path.dirname(__file__),
             '..',
         ),
     )
-    for path in glob('*/requirements.txt'):
-        with open(path, 'w') as f:
-            f.write(f'file:{localpath}')
-    print(LOCALPATH_MSG)
-
-def build(args):
-    DIRNAME = args.dirname if len(DIRS) > 1 else DIRS[0]
-
+    env = copy.deepcopy(os.environ)
+    env['CURRENT_REPO'] = current_repo
     subprocess.check_call([
         'python-appimage',
         'build',
@@ -99,8 +77,7 @@ def build(args):
 
 def main():
     args = parse_args()
-    func = args.__dict__.pop('func')
-    func(args)
+    build(args)
 
 if __name__ == '__main__':
     main()
