120d5afa54718fd0d4c9a4174f8a75875e8173d1
stargateaudio <stargateaudio@noreply.github.com>
2022-10-04 20:19:59 -0700

    make test-all: Various improvements
    + formatting: replace tabs with spaces
    + replace Debian image with Alpine
    + add docstrings

--- tools/test-all-helper.sh
+++ tools/test-all-helper.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -xe
 
+# Helper script to be run inside each containre by test-all-py-versions.sh
+# Runs the unit tests using the current version of Python, with a venv
+# that is stored in the source code folder on the host machine
+
 cd /src
 pyver=$(python3 -c "
 import sys
@@ -10,7 +14,7 @@ workdir=".test-all-workdir/${pyver}"
 logfile="${workdir}/install.log"
 
 if [ ! -d "$workdir" ]; then
-	python3 -m venv "$workdir"
+    python3 -m venv "$workdir"
 fi
 
 export PATH="$(pwd)/${workdir}/bin":"${PATH}"
@@ -19,12 +23,12 @@ date >> "${logfile}"
 which python3 >> "${logfile}"
 which pip3 >> "${logfile}"
 pip3 install \
-	-r requirements/devel.txt \
-	-r requirements/common.txt \
+    -r requirements/devel.txt \
+    -r requirements/common.txt \
     -r requirements/cli.txt \
     -r requirements/rest.txt \
     -r requirements/test.txt \
-	>> "${logfile}"
+    >> "${logfile}"
 
 PT_EXCLUDE_LIBS=ALL pip3 install -e . >> "${logfile}"
 
--- tools/test-all-py-versions.sh
+++ tools/test-all-py-versions.sh
@@ -1,16 +1,22 @@
 #!/bin/sh -xe
 
+# Helper script called by:
+#     make test-all
+# Spawn a Docker container from the official Python images for earch
+# version of Python that your application supports, and run the unit tests
+# to ensure compatibility with each Python version
+
 DOCKER=podman
 
 cd "$(dirname -- ${BASH_SOURCE[0]:-${0:A:h}})/.."
 pwd
 
 for version in {7,8,9,10}; do
-	$DOCKER run \
-		-v ".:/src" \
-		-e pyversion="py3${version}" \
-		--entrypoint 'sh' \
-		python:3.${version}-bullseye "/src/tools/test-all-helper.sh"
+    $DOCKER run \
+        -v ".:/src" \
+        -e pyversion="py3${version}" \
+        --entrypoint 'sh' \
+        python:3.${version}-alpine "/src/tools/test-all-helper.sh"
 done
 
 echo 'Finished!'
