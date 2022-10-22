1a180bc77e84e64fd65766ae8d66949a1726e611
Anon McHacker <anonymous@noreply.example.com>
2022-10-22 00:27:13 -0700

    Fix strange wording

--- src/pytemplate_cli/util/required.py
+++ src/pytemplate_cli/util/required.py
@@ -4,9 +4,10 @@
 import sys
 
 
+_MSG = """
+Error: you must select a subcommand.  See --help for available subcommands.
+"""
+
 def required_subcommand(**kwargs):
-    print(
-        "Error: you must select an additional subcommand.  See the --help "
-        "for your subcommand."
-    )
+    print(_MSG)
     sys.exit(1)
