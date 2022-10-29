db2c4ae49eac68bfabcea5444d363dc7b911fa28
stargateaudio <stargateaudio@noreply.github.com>
2022-10-29 15:09:49 -0700

    Fix Windows and maybe MacOS

--- src/pytemplate/resources.py
+++ src/pytemplate/resources.py
@@ -2,6 +2,7 @@ try:
     from importlib.resources import files
 except ImportError:  # pragma: no cover
     from importlib_resources import files
+import os
 
 
 def get_resource_path(*files_path: str):
@@ -12,5 +13,29 @@ def get_resource_path(*files_path: str):
     path = files('pytemplate').joinpath('files', *files_path)
     if path.exists():
         return str(path)
+    for path in(
+        os.path.join(
+            os.path.dirname(__file__),
+            '..',
+            '..',
+            'files',
+            *files_path,
+        ),
+        os.path.join(
+            os.path.dirname(sys.executable),
+            'files',
+            *files_path,
+        ),
+        os.path.join(
+            os.path.dirname(sys.executable),
+            '..',
+            'Resources',
+            'files',
+            *files_path,
+        ),
+    ):
+        if os.path.exists(path):
+            return os.path.abspath(path)
+
     raise FileNotFoundError
 
