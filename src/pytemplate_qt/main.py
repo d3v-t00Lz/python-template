e46ea221b1a3e9484c3781fa0424684779495ad4
Anon McHacker <anonymous@noreply.L3337.github.com>
2022-11-06 17:23:57 -0800

    System tray improvements

--- src/pytemplate_qt/main.py
+++ src/pytemplate_qt/main.py
@@ -6,6 +6,13 @@ from pytemplate.resources import get_resource_path
 from pytemplate.setup import setup
 from pytemplate.setup.excepthook import add_excepthook
 from pytemplate.log import LOG
+from .qt import (
+    QApplication,
+    QGuiApplication,
+    QPixmap,
+    QSplashScreen,
+    QtCore,
+)
 
 def parse_args():
     parser = argparse.ArgumentParser("pytemplate Qt application")
@@ -19,16 +26,37 @@ def parse_args():
     )
     return parser.parse_args()
 
+def _main_window():
+    global TIMER
+    from .widgets.main_window import MainWindow
+    logo_path = get_resource_path('icons', 'pytemplate.png')
+    pixmap = QPixmap(logo_path).scaled(
+        600,
+        600,
+        transformMode=QtCore.Qt.TransformationMode.SmoothTransformation,
+    )
+    global_vars.SPLASH = QSplashScreen(pixmap)
+    global_vars.SPLASH.show()
+    QApplication.processEvents()
+    global_vars.MAIN_WINDOW = MainWindow()
+    TIMER = QtCore.QTimer()
+    TIMER.setSingleShot(True)
+    TIMER.timeout.connect(
+        lambda: show_main_window(
+            global_vars.MAIN_WINDOW,
+            global_vars.SPLASH,
+        )
+    )
+    TIMER.start(1500)
+
+def _systray():
+    QApplication.setQuitOnLastWindowClosed(False)
+    from .widgets.systray import SystemTray
+    global_vars.SYSTEM_TRAY = SystemTray()
+    global_vars.SYSTEM_TRAY.show()
+
 def _main(*args, **kwargs):
     args = parse_args()
-    # Before Qt imports, to ensure that we can log the outcome
-    from .qt import (
-        QApplication,
-        QGuiApplication,
-        QPixmap,
-        QSplashScreen,
-        QtCore,
-    )
     try:
         QGuiApplication.setHighDpiScaleFactorRoundingPolicy(
             QtCore.Qt.HighDpiScaleFactorRoundingPolicy.PassThrough,
@@ -45,29 +73,9 @@ def _main(*args, **kwargs):
     app = QApplication(sys.argv)
 
     if args.systray:
-        QApplication.setQuitOnLastWindowClosed(False)
-        from .widgets.systray import SystemTray
-        global_vars.SYSTEM_TRAY = SystemTray()
-        global_vars.SYSTEM_TRAY.show()
+        _systray()
     else:
-        from .widgets.main_window import MainWindow
-        logo_path = get_resource_path('icons', 'pytemplate.png')
-        pixmap = QPixmap(logo_path).scaled(
-            600,
-            600,
-            transformMode=QtCore.Qt.TransformationMode.SmoothTransformation,
-        )
-        splash = QSplashScreen(pixmap)
-        splash.show()
-        QApplication.processEvents()
-        window = MainWindow()
-        timer = QtCore.QTimer()
-        timer.setSingleShot(True)
-        timer.timeout.connect(
-            lambda: show_main_window(window, splash)
-        )
-        timer.start(2000)
-        global_vars.MAIN_WINDOW = window
+        _main_window()
     sys.exit(app.exec())
 
 def show_main_window(window, splash):
--- src/pytemplate_qt/widgets/systray.py
+++ src/pytemplate_qt/widgets/systray.py
@@ -15,8 +15,10 @@ class SystemTray(QSystemTrayIcon):
         path = get_resource_path('icons', 'pytemplate-systray.png')
         self.icon = QIcon(path)
         super().__init__(self.icon)
+        # This may not be reliably triggered on all platforms, a known
+        # limitation of QSystemTrayIcon
         self.messageClicked.connect(self.message_clicked)
-        self.activated.connect(self.message_clicked)
+        self.activated.connect(self._activated)
         self.menu = QMenu()
         action = self.menu.addAction('Show a message')
         action.triggered.connect(
@@ -37,7 +39,20 @@ class SystemTray(QSystemTrayIcon):
         global_vars.MAIN_WINDOW = MainWindow()
         global_vars.MAIN_WINDOW.show()
 
-    def message_clicked(self, *args, **kwargs):
+    def _activated(self, reason):
+        if reason in (
+            QSystemTrayIcon.ActivationReason.Trigger,
+            QSystemTrayIcon.ActivationReason.Unknown,
+        ):
+            QMessageBox.information(
+                None,
+                "pytemplate",
+                "You clicked the message or icon",
+            )
+        else:
+            LOG.info(f'_activated({reason})')
+
+    def message_clicked(self):
         QMessageBox.information(
             None,
             "pytemplate",
