84af155a0700e60e7bdc0ab07b0ef98767e17b84
Anon McHacker <anonymous@noreply.L3337.github.com>
2023-03-25 13:05:14 -0700

    resources.py: Add missing test coverage

--- test/pytemplate/test_resources.py
+++ test/pytemplate/test_resources.py
@@ -11,3 +11,7 @@ def test_get_resource_path_not_found():
     with pytest.raises(FileNotFoundError):
         get_resource_path('fakedir', 'fakefile')
 
+def test_get_resource_path_not_in_module():
+    path = get_resource_path('icons', 'pytemplate.icns')
+    assert os.path.isfile(path)
+
